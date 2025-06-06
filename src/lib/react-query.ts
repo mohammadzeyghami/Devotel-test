010879f13cf0410d35822e4d99f7e0aaffc7e2ee
mohammad zeyghami <mohammadzeighami197@gmail.com>
2025-06-06 17:03:45 +0330

    feat/add caching

--- src/lib/react-query.ts
+++ src/lib/react-query.ts
@@ -1,4 +1,16 @@
 // src/lib/react-query.ts
 import { QueryClient } from "@tanstack/react-query";
 
-export const queryClient = new QueryClient();
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 10,
+      gcTime: 1000 * 60 * 10,
+      retry: 1,
+      refetchOnMount: false,
+    },
+    mutations: {
+      retry: 0,
+    },
+  },
+});
